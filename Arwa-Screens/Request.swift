68ba676cc4ecef1e1dfe9b2e3b5e38ba4e9ffb4e
Arrwa <61374875+ArwaFuraih@users.noreply.github.com>
2022-01-25 00:28:55 +0300

    Update Request.swift

--- Arwa-Screens/Request.swift
+++ Arwa-Screens/Request.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct Request: View {
+    @State private var showingAlert = false
+    
     var body: some View {
         
         NavigationView {
@@ -160,15 +162,15 @@ struct Request: View {
                             .fill(/*@START_MENU_TOKEN@*/Color(red: 0.902, green: 0.871, blue: 0.843)/*@END_MENU_TOKEN@*/)
                             .cornerRadius(15)
                             .frame(width: 77, height: 69)
-                        Image ("Charles")
+                        Image ("Julia")
                         }
                         VStack{
                             Text("Julia Garlfelder")
                                 .font(.body)
-                                .padding(.leading, -85)
+                                .padding(.leading, -60)
                             
                             Text("____________________")
-                            Text("Large Classic Chanel       20,000$")
+                            Text("Hermes Handbag         50,000$")
                                 .font(.caption2)
                                 .foregroundColor(Color.gray)
                                 .multilineTextAlignment(.leading)
@@ -183,35 +185,53 @@ struct Request: View {
                                 Text("Request Date :")
                                     .multilineTextAlignment(.leading)
                                     .padding(.leading, -50)
-                                    Text("4/4/2021")
+                                    Text("16/2/2021")
                                         .foregroundColor(Color.gray)
                                 }
                                 HStack{
-                                Text("Request Status : ")
+                                Text("Request Status :")
                                     .multilineTextAlignment(.leading)
-                                    .padding(.leading, -3)
+                                    .padding(.leading, -15)
                                     ZStack{
                                         RoundedRectangle(cornerRadius: 11)
-                                            .foregroundColor(Color(red: 0.788, green: 0.872, blue: 0.824))
+                                            .foregroundColor(Color(red: 0.884, green: 0.628, blue: 0.628))
                                                     .frame(width: 147, height: 32)
-                                        Text("Accepted")
+                                        Text("Declined")
                                             .font(.callout)
 
                                     }//pending shape
                                     
                                 }
                                 .padding(.bottom, 20.0)
-
+ 
+                                HStack{
+                                    
+                                    ZStack{
+                                        RoundedRectangle(cornerRadius: 11)
+                                            .stroke(Color.gray, lineWidth: 2)
+                                                    .frame(width: 41, height: 39)
+                                        Image(systemName:"trash").foregroundColor(Color.gray)
+                                    }
+                                    
                                 ZStack{
-                                    
-                                    Rectangle()
-                                    .cornerRadius(15)
-                                    .frame(width: 267, height: 39)
-                                    Text("Complete your order")
-                                        .foregroundColor(Color.white)
-                                    
+                                    Button("Declining Reasons") {
+                                                showingAlert = true
+                                            }
+                                            .frame(width: 213, height: 39)
+                                            .background(Color.black)
+                                            .foregroundColor(Color.white)
+                                            .cornerRadius(15)
+
+                                            .alert(isPresented:$showingAlert) {
+                                                Alert(
+                                                    title: Text("Order Declined "),
+                                                    message: Text("Sorry item was out of stuck"),
+                                                    dismissButton: .default(Text("Okay"))
+                                                )
+                                             }//alert
+                                }//Zstack
                                 }
-                            }
+                            }//Vstack 
                             RoundedRectangle(cornerRadius: 16)
                                         .stroke(Color(red: 0.902, green: 0.871, blue: 0.843), lineWidth: 2)
                                         .frame(width: 322, height: 268)
