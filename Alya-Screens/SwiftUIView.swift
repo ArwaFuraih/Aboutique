084b433a7b2f3a7e60f4e7c9d618fbdd11a4ed6c
ALYAA7 <96816338+ALYAA7@users.noreply.github.com>
2022-01-28 12:17:23 +0300

    Add files via upload

---
+++ Alya-Screens/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  WORK
+//
+//  Created by alya almutairi on 16/05/1443 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
